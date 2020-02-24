6f7be2e25027658b908ce0dcf6b15966adc93efa
martylanger <langer.marty@gmail.com>
2020-02-24 10:47:29 -0500

    Add curl scripts for CRUD actions, fix typo
    Add curl-scripts/elections/ directory with create, destroy, index, show,
      and update curl scripts
    Fix typo in models/election.rb - move colon

--- app/models/election.rb
+++ app/models/election.rb
@@ -1,3 +1,3 @@
 class Election < ApplicationRecord
-  belongs_to: user
+  belongs_to :user
 end
---
+++ curl-scripts/elections/create.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+curl "http://localhost:4741/elections" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "election": {
+      "name": "'"${NAME}"'"
+      "voting_method": "'"${VOTING_METHOD}"'"
+      "option1": "'"${OPTION1}"'"
+      "option2": "'"${OPTION2}"'"
+      "option3": "'"${OPTION3}"'"
+      "option4": "'"${OPTION4}"'"
+      "option5": "'"${OPTION5}"'"
+      "option6": "'"${OPTION6}"'"
+      "option7": "'"${OPTION7}"'"
+      "option8": "'"${OPTION8}"'"
+      "option9": "'"${OPTION9}"'"
+      "results": "'"${RESULTS}"'"
+    }
+  }'
+
+echo
